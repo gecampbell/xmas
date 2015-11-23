e6f4901813ab2fdbce51fdc7a67176ee8e5fb256
Glen Campbell <glen@broadpool.com>
2015-11-23 11:53:46 -0800

    added js

--- publish.sh
+++ publish.sh
@@ -4,6 +4,7 @@ aws s3 sync . s3://starbucks.christmas \
     --delete \
     --exclude '*.*' \
     --include '*.html' \
+    --include '*.js' \
     --cache-control 'max-age=600' \
     --content-type 'text/html; charset=utf-8'
 
